dc6203554412662d2b3a10c9f2b28c08f10af8ed
Navy <sonlmz@gmail.com>
2018-02-17 21:11:51 +0800

    add source

---
+++ src/main/java/sonlmz/crypto/MyBase64.java
@@ -0,0 +1,15 @@
+package sonlmz.crypto;
+
+/**
+ * Created by Le Minh Son on 17/2/18.
+ */
+public class MyBase64 {
+    public String encode(String text) {
+        return "1";
+    }
+
+    public String decode(String text) {
+        return "1";
+    }
+
+}
---
+++ src/test/java/sonlmz/crypto/MyBase64Test.java
@@ -0,0 +1,21 @@
+package sonlmz.crypto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Le Minh Son on 17/2/18.
+ */
+public class MyBase64Test {
+    @Test
+    public void encode() throws Exception {
+
+    }
+
+    @Test
+    public void decode() throws Exception {
+
+    }
+
+}
