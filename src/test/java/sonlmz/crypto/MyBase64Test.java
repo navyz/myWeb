08f1f8e7b1e0fb4c873ab6d2c3bffbde7205828e
Navy <sonlmz@gmail.com>
2018-02-18 00:30:43 +0800

    d

--- src/test/java/sonlmz/crypto/MyBase64Test.java
+++ src/test/java/sonlmz/crypto/MyBase64Test.java
@@ -8,10 +8,10 @@ import static org.junit.Assert.*;
  * Created by Le Minh Son on 17/2/18.
  */
 public class MyBase64Test {
-    /*@Test
+    @Test
     public void encode() throws Exception {
         assert MyBase64.encode("") == "0";
-    }*/
+    }
 
     @Test
     public void decode() throws Exception {
