6c97360daaf2e9defed1b0036fa569ab567bfae1
Navy <sonlmz@gmail.com>
2018-02-18 00:34:13 +0800

    e

--- src/main/java/sonlmz/crypto/MyBase64.java
+++ src/main/java/sonlmz/crypto/MyBase64.java
@@ -1,5 +1,7 @@
 package sonlmz.crypto;
 
+import java.util.Base64;
+
 /**
  * Created by Le Minh Son on 17/2/18.
  */
@@ -8,11 +10,11 @@ public class MyBase64 {
     private MyBase64() {}
 
     public static String encode(String text) {
-        return "1";
+        return new String(Base64.getEncoder().encode(text.getBytes()));
     }
 
-    public static String decode(String text) {
-        return "1";
+    public static String decode(String cipher) {
+        return new String(Base64.getDecoder().decode(cipher.getBytes()));
     }
 
 }
